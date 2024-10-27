04f002150de964e72819a216e6719c733d1b4834
Joe Sharp <ratracejoe@hotmail.com>
2024-10-27 13:59:14 +0000

    multiple import protection

--- game-of-life.h
+++ game-of-life.h
@@ -1,6 +1,9 @@
+#ifndef GAME_OF_LIFE_H
+#define GAME_OF_LIFE_H
 struct GameOfLife {
 	int contents[10][10];
 };
 
 struct GameOfLife createBoard(int rows, int cols);
 void drawBoard(struct GameOfLife gol);
+#endif
